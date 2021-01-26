2bd1711de2822343fa53b2ce23da9a46a6d13cb7
Stijn <debackere@strw.leidenuniv.nl>
2021-01-26 10:47:14 +0100

    Ensure that value has shape.

--- simulation_slices/operations.py
+++ simulation_slices/operations.py
@@ -66,6 +66,7 @@ def slice_particle_list(
 
     for idx in np.unique(slice_idx):
         for prop, value in properties.items():
+            value = np.atleast_1d(value)
             if value.shape[-1] == len(slice_idx):
                 slice_dict[prop][idx].append(value[..., slice_idx == idx])
             elif value.shape[-1] == 1:
