Fix periodic boundary condition distance calculation.

simulation_slices/map_tools.py
@@ -14,8 +14,8 @@ def pixel_to_pix_id(pixel, num_pix):
     return pixel[0] + pixel[1] * num_pix
 
 
-def diff(x, y, box_size):
-    """Return the vector x - y, taking into account periodic boundary
+def min_diff(x, y, box_size):
+    """Return the minimum vector x - y, taking into account periodic boundary
     conditions.
 
     Parameters
@@ -34,7 +34,7 @@ def diff(x, y, box_size):
         vector x - y
 
     """
-    return np.mod(x - y, box_size)
+    return np.mod(x - y + box_size / 2, box_size) - box_size / 2
 
 
 def dist(x, y, box_size, axis=0):
@@ -57,4 +57,5 @@ def dist(x, y, box_size, axis=0):
         distance between x and y
 
     """
-    return np.linalg.norm(np.mod(x - y, box_size / 2), axis=axis)
+    dx = min_diff(x=x, y=y, box_size=box_size)
+    return np.linalg.norm(dx, axis=axis)

simulation_slices/operations.py
@@ -131,11 +131,11 @@ def coords_to_map(
     A_pix = map_res**2
 
     # convert the coordinates to the pixel coordinate system
-    map_origin = map_tools.diff(np.atleast_1d(map_center), map_size / 2, box_size)
+    map_origin = map_tools.min_diff(np.atleast_1d(map_center), map_size / 2, box_size)
 
     # compute the offsets w.r.t the map_origin, taking into account
     # periodic boundary conditions
-    coords_origin = map_tools.diff(coords, map_origin.reshape(2, 1), box_size)
+    coords_origin = map_tools.min_diff(coords, map_origin.reshape(2, 1), box_size)
 
     # get the x and y values of the pixelated maps
     x_pix = get_coords_slices(coords=coords_origin, slice_size=map_res, slice_axis=0)