a6109bd8934e9d5f11f60c4aec558fd4c21dfcd7
Stijn <debackere@strw.leidenuniv.nl>
2021-02-03 17:21:11 +0000

    Add aperture mass analysis functions.

---
+++ simulation_slices/analysis.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+import simulation_slices.map_tools as map_tools
+import simulation_slices.utilities as util
+
+PI_INV = 1. / np.pi
+
+
+def filter_zeta(R, sigma, A_pix, R1, R2, Rm, **kwargs):
+    """Convergence map filter for zeta_c statistic of Clowe et al.
+    (1998)."""
+    sigma_R1 = np.sum(
+        PI_INV / R1**2 * sigma[np.where(R <= R1)]
+    ) / A_pix
+    sigma_R2_Rm = np.sum(
+        -PI_INV / (Rm**2 - R2**2) * sigma[np.where((R > R2) & (R < Rm))]
+    ) / A_pix
+
+    return sigma_R1 + sigma_R2_Rm
+
+
+def M_ap_from_sigma_map(
+        sigma_map, sigma_crit, filt, pix_scale, pix_0, **filt_kwargs):
+    """Filter sigma_map around x_0 with filter filt(pix - pix_0).
+
+    Parameters
+    ----------
+    sigma_map : (n_pix, n_pix) array
+        projected mass map with same units as sigma_crit
+    sigma_crit : float
+        critical surface mass density
+    filt : callable
+        filter that takes a distance R - R_0 as first argument
+    pix_scale : float
+        physical scale of pixels in length units matching sigma_crit
+    x_0 : (2,) array
+        pixel to center filter on
+
+    Returns
+    -------
+    M_ap : float
+        mass contained within filt around x_0
+    """
+    num_pix = sigma_map.shape[0]
+    pixels = map_tools.pix_id_to_pixel(np.arange(num_pix**2), num_pix)
+
+    R = np.linalg.norm(pixels - pix_0.reshape(2, 1)) * pix_scale
+    A_pix = pix_scale**2
+    M_ap = filt(R, sigma, A_pix, **filt_kwargs)
+    return M_ap
