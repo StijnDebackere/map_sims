Fix aperture mass calculation.

simulation_slices/analysis.py
@@ -45,7 +45,8 @@ def M_ap_from_sigma_map(
     pixels = map_tools.pix_id_to_pixel(
         np.arange(num_pix_side**2), num_pix_side)
 
-    R = np.linalg.norm(pixels - pix_0.reshape(2, 1)) * pix_scale
+    R_pix = np.linalg.norm(pixels - pix_0.reshape(2, 1), axis=0)
+    R = (R_pix * pix_scale).reshape(num_pix_side, num_pix_side)
     A_pix = pix_scale**2
     M_ap = filt(R, sigma_map, A_pix, **filt_kwargs)
     return M_ap

simulation_slices/map_tools.py
@@ -5,7 +5,7 @@ def pix_id_to_pixel(pix_id, num_pix_side):
     """Convert pix_id = i + j * num_pix_side  to pixel (i, j)."""
     if ((pix_id >= num_pix_side**2) | (pix_id < 0)).any():
         raise ValueError('pix_id should be in [0, num_pix_side**2)')
-    return np.array([[pix_id % num_pix_side], [pix_id // num_pix_side]])
+    return np.array([pix_id % num_pix_side, pix_id // num_pix_side])
 
 
 def pixel_to_pix_id(pixel, num_pix_side):