6e53a959ee4a200d4b19e576509fd0693963f56a
Stijn <debackere@strw.leidenuniv.nl>
2021-02-17 17:10:03 +0000

    Compare single file and multiple file reads from multiprocessing.

--- tests/test_hdf5_io.py
+++ tests/test_hdf5_io.py
@@ -1,9 +1,13 @@
+from functools import wraps
 import multiprocessing
 import os
+from pathlib import Path
 import time
 
 import h5py
 import numpy as np
+
+TEST_DIR = Path(__file__).parent
 
 
 def time_this(func, pid=False):
@@ -28,32 +32,32 @@ def on_queue(queue, func, *args, **kwargs):
     queue.put([os.getpid(), res, dt])
 
 
-def generate_random_file():
+def generate_random_file(i=0, size=int(1e7)):
     """Generate large-ish file with random data."""
-    with h5py.File('test.asdf', 'a') as f:
+    with h5py.File(TEST_DIR / f'test_{i:03d}.hdf5', 'a') as f:
         f.create_dataset(
-            'PartType0/Coordinates', data=np.random.uniform(0, 400, (3, 1e7))
+            'PartType0/Coordinates', data=np.random.uniform(0, 400, (3, size))
         )
         f.create_dataset(
-            'PartType0/Masses', data=np.random.uniform(1e-2, 1e4, (1e7,))
+            'PartType0/Masses', data=np.random.uniform(1e-2, 1e4, (size,))
         )
         f.create_dataset(
-            'PartType1/Coordinates', data=np.random.uniform(0, 400, (3, 1e7))
+            'PartType1/Coordinates', data=np.random.uniform(0, 400, (3, size))
         )
         f.create_dataset(
-            'PartType1/Masses', data=np.random.uniform(1e-2, 1e4, (1e7,))
+            'PartType1/Masses', data=np.random.uniform(1e-2, 1e4, (size,))
         )
 
 
-def heavy_operations(h5file_name):
+def heavy_operations(h5file_name, sort_axis=0):
     with h5py.File(h5file_name, 'r') as f:
         coords = f['PartType0/Coordinates'][:]
         masses = f['PartType0/Masses'][:]
 
-    sort_idx = np.argsort(coords)
-    return np.log10(coords[sort_idx] / masses[sort_idx])
+    sort_idx = np.argsort(coords[sort_axis])
+    return np.log10(coords[:, sort_idx] / masses[sort_idx])
 
-def test_multiple_read_single_file(n_cpus=10):
+def multiple_read_single_file(n_cpus=10):
     out_q = multiprocessing.Queue()
     procs = []
     for _ in range(n_cpus):
@@ -61,7 +65,7 @@ def test_multiple_read_single_file(n_cpus=10):
             target=on_queue,
             args=(out_q, heavy_operations),
             kwargs={
-                'h5file_name': 'test.asdf',
+                'h5file_name': str(TEST_DIR / 'test_000.hdf5'),
             }
         )
         procs.append(proc)
@@ -69,11 +73,50 @@ def test_multiple_read_single_file(n_cpus=10):
 
     results = []
     for _ in range(n_cpus):
-        results.append(out_q.get()[0, 2])
+        results.append(out_q.get())
 
     for proc in procs:
         proc.join()
 
     results.sort()
-    times = [r[1] for r in results]
+    times = np.array([r[2] for r in results])
+    return times
+
+
+def multiple_read_different_file(n_cpus=10):
+    out_q = multiprocessing.Queue()
+    procs = []
+    for i in range(n_cpus):
+        proc = multiprocessing.Process(
+            target=on_queue,
+            args=(out_q, heavy_operations),
+            kwargs={
+                'h5file_name': str(TEST_DIR / f'test_{i:03d}.hdf5'),
+            }
+        )
+        procs.append(proc)
+        proc.start()
+
+    results = []
+    for _ in range(n_cpus):
+        results.append(out_q.get())
+
+    for proc in procs:
+        proc.join()
+
+    results.sort()
+    times = np.array([r[2] for r in results])
+    return times
+
+
+def test_multiple_read(mode='single', n_runs=20, n_cpus=32):
+    times = np.empty((n_runs, n_cpus), dtype=float)
+    if mode == 'single':
+        for i in range(n_runs):
+            times[i] = multiple_read_single_file(n_cpus=n_cpus)
+
+    elif mode == 'different':
+        for i in range(n_runs):
+            times[i] = multiple_read_single_file(n_cpus=n_cpus)
+
     return times
