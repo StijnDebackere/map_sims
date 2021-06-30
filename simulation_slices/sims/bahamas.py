8e4bed4a780f9e71922cd03b28ae13b721e7939e
Stijn <debackere@strw.leidenuniv.nl>
2021-06-30 19:35:26 +0200

    Add full map straight from BAHAMAS snapshot data.

--- simulation_slices/sims/bahamas.py
+++ simulation_slices/sims/bahamas.py
@@ -849,3 +849,205 @@ def save_maps_los(
     # still need to close the HDF5 files
     map_file.close()
     return map_name
+
+
+def save_full_maps(
+    sim_dir: str,
+    snapshot: int,
+    slice_axes: int,
+    box_size: u.Quantity,
+    map_types: List[str],
+    map_pix: int,
+    save_dir: str,
+    map_name_append: str = "",
+    downsample: bool = False,
+    downsample_factor: float = None,
+    overwrite: bool = False,
+    swmr: bool = False,
+    method: str = None,
+    n_ngb: int = 30,
+    verbose: bool = False,
+    logger: util.LoggerType = None,
+    **kwargs,
+) -> List[str]:
+    """Project full simulation in a map of (map_pix, map_pix) for slice_axes.
+
+    Parameters
+    ----------
+    sim_dir : str
+        directory of the simulation
+    snapshot : int
+        snapshot to look at
+    slice_axes : int
+        axis to slice along [x=0, y=1, z=2]
+    box_size : astropy.units.Quantity
+        size of simulation
+    map_pix : int
+        square root of number of pixels in map
+    save_dir : str
+        directory to save map files to
+    map_name_append : str
+        optional extra to append to filenames
+    overwrite : bool
+        overwrite map_file if already exists
+    swmr : bool
+        enable single writer multiple reader mode for map_file
+    method : str ["sph", "bin"]
+        method for map projection: sph smoothing with n_ngb neighbours or 2D histogram
+    n_ngb : int
+        number of neighbours to determine SPH kernel size
+    verbose : bool
+        show progress bar
+
+    Returns
+    -------
+    saves maps to {save_dir}/{slice_axis}_maps_{coords_name}{map_name_append}_{snapshot:03d}.hdf5
+
+    """
+    slice_axes = np.atleast_1d(slice_axes)
+    snap_info = Gadget(
+        model_dir=sim_dir,
+        file_type="snap",
+        snapnum=snapshot,
+        units=True,
+        comoving=True,
+    )
+
+    # read in the Mpc unit box_size
+    box_size = snap_info.boxsize
+    h = snap_info.h
+
+    # ensure that save_dir exists
+    if save_dir is None:
+        save_dir = util.check_path(snap_info.filename).parent / "maps"
+    else:
+        save_dir = util.check_path(save_dir)
+
+    fnames = []
+    map_files = {}
+    for slice_axis in slice_axes:
+        map_name = map_gen.get_map_name(
+            save_dir=save_dir,
+            slice_axis=slice_axis,
+            snapshot=snapshot,
+            method=method,
+            map_thickness=box_size,
+            coords_name="",
+            map_name_append=map_name_append,
+            downsample=downsample,
+            downsample_factor=downsample_factor,
+            full=True,
+        )
+        map_file = map_layout.create_map_file(
+            map_name=map_name,
+            overwrite=overwrite,
+            close=False,
+            # cannot have swmr since we are adding attributes later
+            swmr=False,
+            slice_axis=slice_axis,
+            box_size=box_size,
+            map_types=map_types,
+            map_size=box_size,
+            map_thickness=box_size,
+            map_pix=map_pix,
+            snapshot=snapshot,
+            n_ngb=n_ngb,
+            maxshape=0,
+            full=True,
+        )
+        map_files[slice_axis] = {
+            "map": {
+                map_type: np.zeros((map_pix, map_pix), dtype=float)
+                for map_type in map_types
+            },
+            "map_name": map_name,
+            "map_file": map_file,
+        }
+        fnames.append(map_name)
+
+    # now loop over all snapshot files and add their particle info
+    # to the correct slice
+    iterator = enumerate(range(snap_info.num_files))
+    if verbose:
+        iterator = tqdm(
+            iterator,
+            desc="Projecting particle files",
+            total=snap_info.num_files,
+        )
+
+    for idx, file_num in iterator:
+        for map_type in map_types:
+            ptype = obs.MAP_TYPES_OPTIONS[map_type]["ptype"]
+            dsets = obs.MAP_TYPES_OPTIONS[map_type]["dsets"]
+            attrs = obs.MAP_TYPES_OPTIONS[map_type].get("attrs", None)
+            func  = obs.MAP_TYPES_OPTIONS[map_type]["func"]
+
+            # only extract extra dsets
+            dsets = list(set(dsets) - set(["coordinates", "masses"]))
+
+            coords = snap_info.read_single_file(
+                i=file_num,
+                var=PROPS_TO_BAHAMAS[ptype]["coordinates"],
+                verbose=False,
+                reshape=True,
+            ).T
+
+            # dark matter does not have the Mass variable
+            # read in M_sun / h
+            if ptype != "dm":
+                masses = snap_info.read_single_file(
+                    i=file_num,
+                    var=PROPS_TO_BAHAMAS[ptype]["masses"],
+                    verbose=False,
+                    reshape=True,
+                )
+            else:
+                masses = np.atleast_1d(snap_info.masses[PTYPES_TO_BAHAMAS[ptype]])
+
+            properties = {"masses": masses}
+            if len(dsets) > 0:
+                extra_props = {
+                    prop: snap_info.read_single_file(
+                        i=file_num,
+                        var=PROPS_TO_BAHAMAS[ptype][prop],
+                        verbose=False,
+                        reshape=True,
+                    ) for prop in dsets
+                }
+            if attrs is not None:
+                for attr in attrs:
+                    extra_props[attr] = getattr(snap_info, attr)
+
+            properties = {**properties, **extra_props}
+
+            # write each slice to a separate file
+            for slice_axis in slice_axes:
+                no_slice_axis = np.arange(0, 3) != slice_axis
+                if method == "bin":
+                    coords_to_map = map_gen.coords_to_map_bin
+                elif method == "sph":
+                    coords_to_map = map_gen.coords_to_map_sph
+                    properties = {**properties, "n_ngb": n_ngb}
+
+                mp = coords_to_map(
+                    coords=coords[no_slice_axis],
+                    map_size=box_size,
+                    map_pix=map_pix,
+                    box_size=box_size,
+                    func=func,
+                    map_center=None,
+                    logger=logger,
+                    **properties
+                )
+                map_files[slice_axis]["map"][map_type] += mp.value
+                if idx == 0:
+                    map_files[slice_axis]["map_file"][map_type].attrs["units"] = str(mp.unit)
+                if idx % 10 == 0:
+                    # save map to map_file and start again at 0
+                    map_files[slice_axis]["map_file"][map_type][()] += map_files[slice_axis]["map"][map_type]
+                    map_files[slice_axis]["map"][map_type] = np.zeros((map_pix, map_pix), dtype=float)
+
+    for slice_axis in slice_axes:
+        map_files[slice_axis]["map_file"].close()
+
+    return fnames
