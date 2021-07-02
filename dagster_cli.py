90ae3e2fac188f2fb1e76444a609c4e10e139bad
Stijn <debackere@strw.leidenuniv.nl>
2021-07-02 09:42:51 +0100

    Split dagster interface up, add cli script for runs.
    - Split solid_factories and add pipeline factory that takes in config
      file.
    - Add cli script to run dagster instance for given config file.

---
+++ dagster_cli.py
@@ -0,0 +1,128 @@
+import argparse
+from pathlib import Path
+
+from dagster import (
+    DagsterInstance,
+    execute_pipeline,
+    pipeline,
+    make_values_resource,
+    ModeDefinition,
+    multiprocess_executor,
+)
+from dagster.core.definitions.reconstructable import build_reconstructable_pipeline
+from dagster.core.storage.fs_io_manager import fs_io_manager
+
+import simulation_slices.tasks as tasks
+from simulation_slices import Config
+
+import dagster_solids as solids
+
+
+parser = argparse.ArgumentParser(description='Run a dagster pipeline from config_filename.')
+parser.add_argument(
+    "config_filename",
+    default="/hpcdata0/simulations/BAHAMAS/extsdeba/batch_files/batch_bahamas_test.toml",
+    type=str,
+    help="configuration filename"
+)
+parser.add_argument(
+    "-n", "--max_cpus",
+    default=8,
+    type=int,
+    help="maximum number of concurrent processes"
+)
+parser.add_argument("--slice-sims", dest="slice_sims", action="store_true")
+parser.add_argument("--no-slice-sims", dest="slice_sims", action="store_false")
+parser.set_defaults(slice_sims=False)
+parser.add_argument("--save-coords", dest="save_coords", action="store_true")
+parser.add_argument("--no-save-coords", dest="save_coords", action="store_false")
+parser.set_defaults(save_coords=False)
+parser.add_argument("--map-sims", dest="map_sims", action="store_true")
+parser.add_argument("--no-map-sims", dest="map_sims", action="store_false")
+parser.set_defaults(map_sims=True)
+parser.add_argument("--project-full", dest="project_full", action="store_true")
+parser.add_argument("--no-project-full", dest="project_full", action="store_false")
+parser.set_defaults(project_full=True)
+parser.add_argument("--project-los", dest="project_los", action="store_true")
+parser.add_argument("--no-project-los", dest="project_los", action="store_false")
+parser.set_defaults(project_los=False)
+
+
+def pipeline_factory(config_filename: str):
+    @pipeline(
+        name="process_simulations",
+        mode_defs=[
+            ModeDefinition(
+                executor_defs=[multiprocess_executor],
+                resource_defs={
+                    "io_manager": fs_io_manager,
+                    "settings": make_values_resource(
+                        slice_sims=bool,
+                        save_coords=bool,
+                        map_sims=bool,
+                        project_full=bool,
+                        project_los=bool,
+                    ),
+                },
+            )
+        ],
+        description="Pipeline to generate observable maps from simulations.",
+    )
+    def pipeline_with_config():
+        solid_output_handles = []
+        cfg = Config(config_filename)
+
+        for sim_idx in range(cfg._n_sims):
+            for idx_snap, snapshot in enumerate(cfg.snapshots[sim_idx]):
+                slice_sim = solids.slice_sim_solid_factory(
+                    sim_idx=sim_idx, snapshot=snapshot, cfg=cfg
+                )
+                save_coords = solids.save_coords_solid_factory(
+                    sim_idx=sim_idx, snapshot=snapshot, cfg=cfg
+                )
+
+                coords_file = str(cfg.coords_files[sim_idx][idx_snap])
+                map_sim = solids.map_sim_solid_factory(
+                    sim_idx=sim_idx, snapshot=snapshot, coords_file=coords_file, cfg=cfg
+                )
+
+                solid_output_handles.append(map_sim(save_coords(slice_sim())))
+
+    return pipeline_with_config
+
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    dict_args = vars(args)
+
+    reconstructable_pipeline = build_reconstructable_pipeline(
+        "run_dagster",
+        "pipeline_factory",
+        (),
+        {"config_filename": dict_args["config_filename"]},
+    )
+    execute_pipeline(
+        reconstructable_pipeline,
+        instance=DagsterInstance.get(),
+        run_config={
+            "execution": {
+                "multiprocess": {
+                    "config": {
+                        "max_concurrent": dict_args["max_cpus"],
+                    }
+                }
+            },
+            # "loggers": {"console": {"config": {"log_level": "INFO"}}},
+            "resources": {
+                "settings": {
+                    "config": {
+                        "slice_sims": dict_args["slice_sims"],
+                        "save_coords": dict_args["save_coords"],
+                        "map_sims": dict_args["map_sims"],
+                        "project_full": dict_args["project_full"],
+                        "project_los": dict_args["project_los"],
+                    },
+                },
+            },
+        },
+    )
---
+++ dagster_solids.py
@@ -0,0 +1,173 @@
+from dagster import (
+    solid,
+    InputDefinition,
+    Output,
+    OutputDefinition,
+    AssetMaterialization,
+    AssetKey,
+    EventMetadataEntry,
+    Nothing,
+)
+
+import simulation_slices.tasks as tasks
+from simulation_slices import Config
+
+
+# inspired by https://stackoverflow.com/q/61330816/
+def slice_sim_solid_factory(sim_idx: int, snapshot: int, cfg: Config):
+    @solid(
+        name=str(
+            f"slice_sim_{str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}_{snapshot:03d}"
+        ),
+        description=f"Slice {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')} for {snapshot=}.",
+        required_resource_keys={"settings"},
+    )
+    def _slice_sim(context) -> Nothing:
+        if context.resources.settings["slice_sims"]:
+            context.log.info(
+                f"Start slicing {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')} for {snapshot=}"
+            )
+            if cfg.logging:
+                logger = context.log
+            else:
+                logger = None
+
+            fnames = tasks.slice_sim(
+                sim_idx=sim_idx, snapshot=snapshot, config=cfg, logger=logger
+            )
+            context.log.info(
+                f"Finished slicing {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')} for {snapshot=}"
+            )
+
+            for idx, fname in enumerate(fnames):
+                yield AssetMaterialization(
+                    asset_key=AssetKey(
+                        f"slice_sim_{str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}_{idx}_{snapshot:03d}"
+                    ),
+                    description=f"Slice file {idx} for {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')} for {snapshot=}",
+                    metadata_entries=[EventMetadataEntry.path(fname, "file path")],
+                )
+        else:
+            context.log.info(
+                f"Skipping slicing {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}"
+            )
+
+        yield Output(None)
+
+    return _slice_sim
+
+
+def save_coords_solid_factory(sim_idx: int, snapshot: int, cfg: Config):
+    @solid(
+        name=str(
+            f"save_coords_{str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}_{snapshot:03d}"
+        ),
+        input_defs=[InputDefinition("ready", dagster_type=Nothing)],
+        description=f"Save coordinates for {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')} for {snapshot=}.",
+        required_resource_keys={"settings"},
+    )
+    def _save_coords(context) -> Nothing:
+        if context.resources.settings["save_coords"]:
+            context.log.info(
+                f"Start saving coordinates for {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')} for {snapshot=}"
+            )
+            if cfg.logging:
+                logger = context.log
+            else:
+                logger = None
+
+            fname = tasks.save_coords(
+                sim_idx=sim_idx, snapshot=snapshot, config=cfg, logger=logger
+            )
+
+            context.log.info(
+                f"Finished saving coordinates for {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')} for {snapshot=}"
+            )
+
+            yield AssetMaterialization(
+                asset_key=AssetKey(
+                    f"save_coords_{str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}_{snapshot:03d}"
+                ),
+                description=f"Coordinates for {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')} for {snapshot=}",
+                metadata_entries=[EventMetadataEntry.path(fname, "file path")],
+            )
+        else:
+            context.log.info(
+                f"Skipping saving coordinates for {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}"
+            )
+
+        yield Output(None)
+
+    return _save_coords
+
+
+def map_sim_solid_factory(sim_idx: int, snapshot: int, coords_file: str, cfg: Config):
+    @solid(
+        # needed output is list of AssetKeys from slice_sim
+        input_defs=[InputDefinition("ready", dagster_type=Nothing)],
+        name=str(
+            f"map_sim_{str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}_{snapshot:03d}"
+        ),
+        description=f"Produce maps of {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')} for {snapshot=}.",
+        required_resource_keys={"settings"},
+    )
+    def _map_sim(context) -> Nothing:
+        if context.resources.settings["map_sims"]:
+            context.log.info(
+                f"Start mapping simulation {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}"
+            )
+            if cfg.logging:
+                logger = context.log
+            else:
+                logger = None
+
+            fnames = []
+            if context.resources.settings["project_full"]:
+                fnames = tasks.map_full(
+                    sim_idx=sim_idx,
+                    config=cfg,
+                    snapshot=snapshot,
+                    logger=logger,
+                )
+                fnames.append(*fnames)
+            elif context.resources.settings["project_los"]:
+                for slice_axis in cfg.slice_axes:
+                    fname = tasks.map_los(
+                        sim_idx=sim_idx,
+                        snapshot=snapshot,
+                        slice_axis=slice_axis,
+                        coords_file=coords_file,
+                        config=cfg,
+                        logger=logger,
+                    )
+                    fnames.append(fname)
+            else:
+                for slice_axis in cfg.slice_axes:
+                    fname = tasks.map_coords(
+                        sim_idx=sim_idx,
+                        config=cfg,
+                        snapshot=snapshot,
+                        slice_axis=slice_axis,
+                        coords_file=coords_file,
+                        logger=logger,
+                    )
+                    fnames.append(fname)
+
+            context.log.info(
+                f"Finished mapping simulation {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}"
+            )
+
+            for idx, fname in enumerate(fnames):
+                yield AssetMaterialization(
+                    asset_key=f"map_sim_{str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}_{idx}_{snapshot:03d}",
+                    description=f"Maps file {idx} for {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}",
+                    metadata_entries=[EventMetadataEntry.path(fname, "file path")],
+                )
+        else:
+            context.log.info(
+                f"Skipping mapping simulation {str(cfg.sim_dirs[sim_idx]).replace('.', 'p')}"
+            )
+
+        yield Output(None)
+
+    return _map_sim
