1d78f11502afac59e91703a4ccf610091ab4dc22
Stijn <debackere@strw.leidenuniv.nl>
2021-09-09 12:45:02 +0200

    Print extra information for sbatch_single.

--- sbatch_single.sh
+++ sbatch_single.sh
@@ -13,7 +13,6 @@
 
 echo "Activating conda environment simulation slices"
 conda activate simulation_slices
-echo "Running batch job"
 
 # limit the number of threads for pygio
 export OMP_NUM_THREADS=1
@@ -21,13 +20,10 @@ export BATCH_FILE=$1
 export N_START=$2
 export N_STOP=$3
 
+echo "Running batch job for sim_ids=${N_START}-${N_STOP} from ${BATCH_FILE}"
 # run the cli script to generate the full maps for each snapshot for all sims
 # snapshots are passed through SBATCH array
-eval srun -n1 --exclusive python map_sims_cli.py $BATCH_FILE ${SLURM_ARRAY_TASK_ID} \
-     -i {$N_START..$N_STOP} \
-     --save-coords \
-     # --no-save-coords \
-     # --project-full \
-     --no-project-full \
-     &
+# eval srun -n1 --exclusive python map_sims_cli.py $BATCH_FILE ${SLURM_ARRAY_TASK_ID} -i {$N_START..$N_STOP} --save-coords --project-full &
+eval srun -n1 --exclusive python map_sims_cli.py $BATCH_FILE ${SLURM_ARRAY_TASK_ID} -i {$N_START..$N_STOP} --save-coords --no-project-full &
+# eval srun -n1 --exclusive python map_sims_cli.py $BATCH_FILE ${SLURM_ARRAY_TASK_ID} -i {$N_START..$N_STOP} --no-save-coords --project-full &
 wait
