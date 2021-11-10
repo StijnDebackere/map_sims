f5d5419fd15f767b851a16af6fafba7f71e4b404
Stijn <debackere@strw.leidenuniv.nl>
2021-11-10 12:30:12 +0100

    Remove SBATCH args that are passed to sbatch script.

--- sbatch_single.sh
+++ sbatch_single.sh
@@ -2,13 +2,11 @@
 
 #SBATCH -J batch-%j.job
 #SBATCH --partition=all
-#SBATCH --ntasks=8
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=30000
-#SBATCH --array=163,189,247,300,347,401,453,499
 #SBATCH --output=batch-%j.out
 #SBATCH --error=batch-%j.err
-#SBATCH --time=1-00:00:00
+#SBATCH --time=30-00:00:00
 
 
 echo "Activating conda environment simulation slices"
