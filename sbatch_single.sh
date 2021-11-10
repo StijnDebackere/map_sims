6a329b7b6c7632d3c812446426f1046abd1ae737
Stijn <debackere@strw.leidenuniv.nl>
2021-11-10 13:33:06 +0100

    Also include snapshot information in log.

--- sbatch_single.sh
+++ sbatch_single.sh
@@ -9,7 +9,7 @@ export BATCH_FILE=$1
 export N_START=$2
 export N_STOP=$3
 
-echo "Running batch job for sim_ids=${N_START}-${N_STOP} from ${BATCH_FILE}"
+echo "Running batch job for sim_ids=${N_START}-${N_STOP} from ${BATCH_FILE} for snapshot=${SLURM_ARRAY_TASK_ID}"
 # run the cli script to generate the full maps for each snapshot for all sims
 # snapshots are passed through SBATCH array
 # eval srun -n1 --exclusive python map_sims_cli.py ${BATCH_FILE} ${SLURM_ARRAY_TASK_ID} -i {$N_START..$N_STOP} --save-coords --project-full &
