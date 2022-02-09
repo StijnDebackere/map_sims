167891df8fd1382b5b52935a18f1dd4b3a3fb016
Stijn <debackere@strw.leidenuniv.nl>
2022-02-09 18:00:32 +0100

    Change map_sims_cli to run for all sims if no sim_ids are passed.

--- map_sims/maps/scripts/map_sims_cli.py
+++ map_sims/maps/scripts/map_sims_cli.py
@@ -1,3 +1,9 @@
+"""Command-line tool to generate projected mass maps from particle data.
+
+Usage: python map_sims_cli.py config.toml -s XXX YYY ZZZ -i 0 1 2 --project-full --save-info
+
+config.toml should be a toml file matching tasks.Config()
+"""
 #!/usr/bin/env python3
 import argparse
 import os
@@ -18,17 +24,17 @@ parser.add_argument(
 )
 parser.add_argument(
     "-s", "--snapshots",
-    default=[401],
+    default=None,
     type=int,
     nargs="+",
     help="snapshot to evaluate",
 )
 parser.add_argument(
     "-i", "--sim_ids",
-    default=[0],
+    default=None,
     type=int,
     nargs="+",
-    help="index of config_file.sim_dirs to load",
+    help="index of config_file.sim_dirs to load [Default: None, run all simulations in config_filename]",
 )
 parser.add_argument("--project-full", dest="project_full", action="store_true")
 parser.add_argument("--no-project-full", dest="project_full", action="store_false")
@@ -40,14 +46,20 @@ parser.set_defaults(save_info=False)
 
 def main():
     args = parser.parse_args()
+    cfg = tasks.Config(args.config_filename)
 
     # ensure that mira_titan does not claim all resources
     os.environ["OMP_NUM_THREADS"] = "1"
-    snapshots = args.snapshots
-    sim_ids = args.sim_ids
 
-    cfg = tasks.Config(args.config_filename)
-    # convert to strings instead of PosixPaths
+    sim_ids = args.sim_ids
+    if sim_ids is None:
+        sim_ids = range(len(cfg.sim_dirs))
+
+    snapshots = args.snapshots
+    if snapshots is None:
+        raise ValueError("need to specify -s or --snapshots option")
+
+    # convert sim dirs to strings instead of PosixPaths
     sims = [str(d) for d in cfg.sim_dirs]
     slice_axes = cfg.slice_axes
 
